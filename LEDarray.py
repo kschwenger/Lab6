99fb1f107348e1c3ff9c72909ad69217deaf02a2
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-13 01:42:29 +0000

    fuuuck

--- LEDarray.py
+++ LEDarray.py
@@ -12,8 +12,8 @@ class LEDarray():
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
   
-  def display(self, pat):
-    row = 4
+  def display(self, pat): # display given part of a pattern
+    row = 5
     #rows = [1,2,3,4,5,6,7,8] # change this value to pick which row the pattern appears on
     #for row in range(len(rows)):
     self.shifter.shiftByte(~LEDarray.pattern[pat]) # load the row values
