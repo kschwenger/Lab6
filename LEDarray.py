d36285745b4511e3655631ebf0dd81986ae816c5
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-13 00:57:49 +0000

    pattern in class

--- LEDarray.py
+++ LEDarray.py
@@ -3,10 +3,11 @@
 from time import sleep
 from shifter import Shifter
 
-global pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
 class LEDarray():
   'Class for controlling an array of LEDs'
+
+  pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
