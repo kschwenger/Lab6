a515d003ce2e253a87caefc5dee755d69a598620
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 17:26:59 +0000

    test

--- LED8x8.py
+++ LED8x8.py
@@ -7,14 +7,14 @@ from shifter import Shifter # extend by composition
 class LED8x8():
   'Class for controlling an array of LEDs'
 
-  pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+  pattern = [0b00111100] #, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
   
   def display(self): # display given part of a pattern
     while True:
-      for row in range(8):
+      for row in range(len(pattern)):
         self.shifter.shiftByte(~LED8x8.pattern[row]) # load the row values
         self.shifter.shiftByte(1 << (7 - row)) # select the given row
         self.shifter.ping(self.shifter.latchPin)
