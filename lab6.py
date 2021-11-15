c1bb892d982c14499be5b894a1ef7503b8239e67
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 00:48:45 +0000

    row 1

--- lab6.py
+++ lab6.py
@@ -5,7 +5,7 @@ dataPin, latchPin, clockPin = 18, 19, 26
 
 theLEDarray = LEDarray(dataPin, latchPin, clockPin)
 
-sequence = [7, 6, 7, 5, 3, 0, 9]
+sequence = [7, 6, 7, 5, 3, 0, 7]
 
 while True:
   for n in range(len(sequence)):
