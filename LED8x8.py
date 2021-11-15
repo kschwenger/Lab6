8f2732cc92afd927acb03fbc9b6ae7e9af47dd8c
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 17:35:43 +0000

    pattern arg

--- LED8x8.py
+++ LED8x8.py
@@ -8,14 +8,14 @@ class LED8x8():
   'Class for controlling an array of LEDs'
 
   #pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
-  pattern = [0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]
+  #pattern = [0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
   
-  def display(self): # display given part of a pattern
+  def display(self, pattern): # display given part of a pattern
     while True:
-      for row in range(len(self.pattern)):
+      for row in range(len(pattern)):
         self.shifter.shiftByte(~LED8x8.pattern[row]) # load the row values
         self.shifter.shiftByte(1 << (7 - row)) # select the given row
         self.shifter.ping(self.shifter.latchPin)
--- lab6.py
+++ lab6.py
@@ -5,4 +5,6 @@ dataPin, latchPin, clockPin = 18, 19, 26
 
 theLED8x8 = LED8x8(dataPin, latchPin, clockPin)
 
-theLED8x8.display()
+pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+
+theLED8x8.display(pattern)
