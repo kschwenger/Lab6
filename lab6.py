510d82f7df8cbeb3d5fbe29e5a2bb4bed5f361ca
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 18:15:30 +0000

    asfgaf

--- lab6.py
+++ lab6.py
@@ -1,10 +1,23 @@
 from time import sleep
 from LED8x8 import LED8x8
+from random import randint
 
 dataPin, latchPin, clockPin = 18, 19, 26
 
 theLED8x8 = LED8x8(dataPin, latchPin, clockPin)
 
-pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+pattern = [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]  #start empty
+
+x = randint(0, 7) #random starting position
+y = randint(0, 7)
+pattern[y] = 1 << x
+
+#stepX = randint(-1,1)
+#stepY = randint(-1,1)
+
+#x += stepX
+
+
+
 
 theLED8x8.display(pattern)
