b64b1496456936b12cb09ed3a138691250306973
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 22:22:39 +0000

    exception handling

--- LED8x8.py
+++ LED8x8.py
@@ -1,25 +1,24 @@
 #LED8x8 class
 import multiprocessing
 from time import sleep
-from shifter import Shifter # extend by composition
+from shifter import Shifter 
 
 
 class LED8x8(multiprocessing.Process):
   'Class for controlling an array of LEDs'
   
-  pattern = multiprocessing.Array('i',[0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])  #start empty
+  pattern = multiprocessing.Array('i',[0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])  #start with array empty 
 
   def __init__(self, data, latch, clock):
-    self.shifter = Shifter(data, latch, clock)
-    p = multiprocessing.Process(target=self.display, args=(self.pattern,))
-    p.daemon = True
-    p.start()
+    self.shifter = Shifter(data, latch, clock)  #extend by composition
+    d = multiprocessing.Process(target=self.display, args=(self.pattern,))  #define process to run display() with the pattern as arument
+    d.daemon = True   #start daemon process (terminates when main code ends)
+    d.start() 
 
-  
-  def display(self, pattern): # display given part of a pattern
+  def display(self, pattern): #display pattern continuously
     while True:
-      for row in range(len(pattern)):
-        self.shifter.shiftByte(~pattern[row]) # load the row values
-        self.shifter.shiftByte(1 << (7 - row)) # select the given row
-        self.shifter.ping(self.shifter.latchPin)
+      for row in range(len(pattern)): #sequentially populate all rows
+        self.shifter.shiftByte(~pattern[row]) #load the row values
+        self.shifter.shiftByte(1 << (7 - row)) #select the given row
+        self.shifter.ping(self.shifter.latchPin)  #ping the latch pin after sending 2 bytes
         sleep(0.001)
--- lab6.py
+++ lab6.py
@@ -2,34 +2,38 @@ from time import sleep
 from LED8x8 import LED8x8
 from random import randint
 
-dataPin, latchPin, clockPin = 18, 19, 26
+dataPin, latchPin, clockPin = 18, 19, 26  #define SR pins
 
-theLED8x8 = LED8x8(dataPin, latchPin, clockPin)
+theLED8x8 = LED8x8(dataPin, latchPin, clockPin) #create multiprocessing object
 
-x = randint(0, 7) #random starting position
+x = randint(0, 7) #random starting position (x = column, y = row)
 y = randint(0, 7)
 
-while True:
+try:
   
-  #theLED8x8.display()  #display the pattern on the led array
+  while True:
+  
+    if x < 1: #prevent position from going outside of 8x8
+      x += randint(0,1)
+    elif x > 6:  
+      x += randint(-1,0)
+    else: #random step
+      x += randint(-1,1)
+  
+    if y < 1:
+      y += randint(0,1)
+    elif y > 6:
+      y += randint(-1,0)
+    else:
+      y += randint(-1,1)
+  
+    for i in range(8):
+      theLED8x8.pattern[i] = 0b00000000 #reset pattern to turn all LEDs off
+    theLED8x8.pattern[y] = 1 << x #turn on xth bit in yth row 
 
-  if x < 1: #prevent position from going outside of 8x8
-    x += randint(0,1)
-  elif x > 6:  
-    x += randint(-1,0)
-  else: #random step
-    x += randint(-1,1)
-  
-  if y < 1:
-    y += randint(0,1)
-  elif y > 6:
-    y += randint(-1,0)
-  else:
-    y += randint(-1,1)
-  
-  for i in range(8):
-    theLED8x8.pattern[i] = 0b00000000 #reset pattern to turn all LEDs off
-  theLED8x8.pattern[y] = 1 << x #turn on xth bit in yth row 
-
-  sleep(.1)
-#try with multi
+    sleep(.1)
+except KeyboardInterrupt: # if user hits ctrl-C
+except Exception as e:   # catch everything, just in case
+  print(e)               # delete once code is debugged
+  theLED8x8.d.terminate()      # terminate the process
+  theLED8x8.d.join(2)          # wait up to 2 sec for process termination before ending code
