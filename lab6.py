f47759a98c9f68362ee0567dfe808fdd20b8fba3
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 18:28:48 +0000

    ?

--- lab6.py
+++ lab6.py
@@ -6,12 +6,11 @@ dataPin, latchPin, clockPin = 18, 19, 26
 
 theLED8x8 = LED8x8(dataPin, latchPin, clockPin)
 
-pattern = [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]  #start empty
-
 x = randint(0, 7) #random starting position
 y = randint(0, 7)
 
 while True:
+  pattern = [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]  #start empty
   pattern[y] = 1 << x #change the yth row to be x
   theLED8x8.display(pattern)
 
