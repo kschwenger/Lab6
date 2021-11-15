3977d3c09c8632464ab0f45425fc0c1752e2c2df
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-15 18:24:36 +0000

    asefawef

--- lab6.py
+++ lab6.py
@@ -15,5 +15,18 @@ while True:
   pattern[y] = 1 << x #change the yth row to be x
   theLED8x8.display(pattern)
 
-  x += randint(-1,1)
-  y =+ randint(-1,1)
+  if x < 1:
+    x += randint(0,1)
+  elif x > 6:
+    x += randint(-1,0)
+  else:
+    x += randint(-1,1)
+  
+  if y < 1:
+    y += randint(0,1)
+  elif y > 6:
+    y += randint(-1,0)
+  else:
+    y += randint(-1,1)
+  
+  sleep(0.1)
